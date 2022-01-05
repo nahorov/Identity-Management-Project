5dfead82003ab5f3da89a2243db2a7a76b749269
Rohan Puranik <96951713+nahorov@users.noreply.github.com>
2022-01-05 23:48:09 +0530

    Create aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>About Us</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="header">
+        <h2>About Us</h2>
+</div>
+
+<br/>
+<div class="content">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi quis hendrerit dolor magna eget est lorem. Tincidunt lobortis feugiat vivamus at. Leo a diam sollicitudin tempor. Massa tempor nec feugiat nisl. Tellus molestie nunc non blandit massa enim nec dui. Odio facilisis mauris sit amet massa vitae tortor condimentum lacinia. Id volutpat lacus laoreet non curabitur gravida. Gravida cum sociis natoque penatibus et magnis dis parturient montes. Porttitor massa id neque aliquam vestibulum morbi blandit cursus risus. Odio aenean sed adipiscing diam donec adipiscing tristique risus. Imperdiet proin fermentum leo vel orci porta non pulvinar. Enim nunc faucibus a pellentesque sit amet porttitor. Pretium lectus quam id leo in vitae turpis massa. Urna et pharetra pharetra massa massa ultricies mi quis hendrerit. Pulvinar neque laoreet suspendisse interdum consectetur.
+<br/><br/>
+Tincidunt dui ut ornare lectus sit amet est placerat in. Viverra vitae congue eu consequat. Diam quam nulla porttitor massa id neque. Tincidunt eget nullam non nisi. Egestas congue quisque egestas diam. Vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur. Diam maecenas ultricies mi eget. Eget magna fermentum iaculis eu. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc. Vitae turpis massa sed elementum tempus egestas sed. Fringilla ut morbi tincidunt augue. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt.
+</div>
+<br/><br/>
+</body>
+</html>
